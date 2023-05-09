4b63a2a8ea8f0eef59b35a4e21296ec6048751f2
Dr-mnu23 <96422181+Dr-mnu23@users.noreply.github.com>
2023-05-09 10:16:46 +0300

    Update cizim3.js

--- cizim3.js
+++ cizim3.js
@@ -1,24 +1,20 @@
 /*
- * @name Patterns
- * @description Move the cursor over the image to draw with a software tool
- * which responds to the speed of the mouse.
- */
+  * @name Kalıpları
+  * @description Farenin hızına tepki veren bir yazılım aracıyla çizim yapmak için imleci resmin üzerine getirin.
+  */
 function setup() {
   createCanvas(710, 400);
   background(102);
 }
 
 function draw() {
-  // Call the variableEllipse() method and send it the
-  // parameters for the current mouse position
-  // and the previous mouse position
+//variableEllipse() yöntemini çağırın ve ona geçerli fare konumu ve önceki fare konumu için parametreleri gönderin
   variableEllipse(mouseX, mouseY, pmouseX, pmouseY);
 }
 
-// The simple method variableEllipse() was created specifically
-// for this program. It calculates the speed of the mouse
-// and draws a small ellipse if the mouse is moving slowly
-// and draws a large ellipse if the mouse is moving quickly
+// Bu program için özel olarak variableEllipse() basit yöntemi oluşturuldu. 
+// Farenin hızını hesaplar ve fare yavaş hareket ediyorsa küçük bir elips, 
+// hızlı hareket ediyorsa büyük bir elips çizer.
 
 function variableEllipse(x, y, px, py) {
   let speed = abs(x - px) + abs(y - py);
